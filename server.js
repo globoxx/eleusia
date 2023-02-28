f5a050781fa54189c8fe48714f5181fa6a23ab39
Vincent Gürtler <vincent.guertler@gmail.com>
2023-02-28 11:40:28 +0100

    Add possibility to create and join rooms

--- server.js
+++ server.js
@@ -10,37 +10,46 @@ const data = {};
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  socket.on('joinRoom', (roomId, pseudo) => {
-    console.log(`User ${socket.id} joined room ${roomId}`);
-    socket.join(roomId);
-    let is_creator = false;
-    if (!(roomId in data)) {
-      data[roomId] = {
-        counter: 0,
-        creator: socket.id,
-        users: {
-            [pseudo]: {
-                'score': 0
-            }
-        }
+  socket.emit('updateRooms', Object.keys(data));
+
+  socket.on('createRoom', (roomId, pseudo) => {
+    console.log(`User ${socket.id} with pseudo ${pseudo} created new room ${roomId}`);
+    data[roomId] = {
+      counter: 0,
+      creator: pseudo,
+      users: {
+          [pseudo]: {
+              'score': 0
+          }
       }
-      is_creator = true;
-    } else {
+    }
+
+    io.emit('updateRooms', Object.keys(data));
+    console.log(`List of rooms ${Object.keys(data)}`);
+  })
+
+  socket.on('joinRoom', (roomId, pseudo) => {
+    if (roomId in data) {
+      console.log(`User ${socket.id} with pseudo ${pseudo} joined room ${roomId}`);
+      socket.join(roomId);
       data[roomId]['users'][pseudo] = {
         'score': 0
       }
+      const is_creator = pseudo === data[roomId]['creator'];
+      io.to(roomId).emit('roomJoined', data[roomId], is_creator);
+    } else {
+      console.log('ROOM NOT FOUND');
     }
-    socket.emit('roomJoined', data[roomId], is_creator);
   });
 
   socket.on('buttonClick', (roomId, pseudo) => {
-    console.log(`User ${socket.id} clicked button in room ${roomId}`);
+    console.log(`User ${socket.id} with pseudo ${pseudo} clicked button in room ${roomId}`);
     if (roomId in data) {
         data[roomId]['counter']++;
         data[roomId]['users'][pseudo]['score']++;
         io.to(roomId).emit(`counter`, data[roomId]);
     } else {
-        console.log(`User ${socket.id} clicked button in room ${roomId} but this room does not exist !`);
+        console.log(`User ${socket.id} with pseudo ${pseudo} clicked button in room ${roomId} but this room does not exist !`);
     }
   });
 
