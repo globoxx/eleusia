7ec497c00af14f0b68f92cda5b54e1bea8772fad
Vincent Gürtler <vincent.guertler@gmail.com>
2023-03-14 11:26:03 +0100

    Init react version

---
+++ server/server.tsx
@@ -0,0 +1,48 @@
+export interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+export interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+import cors from "cors";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+  cors: {
+    origin: "https://example.com"
+  }
+});
+
+// our localhost port
+const port = 5000
+
+app.use(cors());
+
+// This is what the socket.io syntax is like, we will work this later
+io.on('connection', socket => {
+  console.log('User connected')
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
