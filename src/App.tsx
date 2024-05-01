9b82446b54639427258f2e6d435acfc6442b890b
Vincent Gürtler <vincent.guertler@gmail.com>
2024-05-01 10:49:44 +0200

    Add alert on connection error

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { io, Socket } from "socket.io-client";
 import GameBoard from './components/GameBoard';
 import Home from './components/Home'
 import { RoomData } from '../server';
-import { Box } from '@mui/material';
+import { Alert, Box, Snackbar } from '@mui/material';
 
 const socket: Socket = io()
 
 function App() {
   useEffect(()=>{
-    socket.on('connect', () => console.log(socket.id))
+    socket.on('connect', () => {
+      console.log(socket.id)
+      setConnectionError(false)
+    });
+
     socket.on('connect_error', () => {
-      setTimeout(() => socket.connect(), 5000)
-    })
+      setTimeout(() => {
+          socket.connect();
+      }, 5000);
+      setConnectionError(true)
+    });
 
     socket.on('updateRoomData', (roomData: RoomData) => {
       setRoomData(roomData)
@@ -23,6 +30,7 @@ function App() {
   const [pseudo, setPseudo] = useState('')
   const [room, setRoom] = useState('')
   const [roomData, setRoomData] = useState<RoomData | null>(null)
+  const [connectionError, setConnectionError] = useState(false)
 
   const callbackPseudoChange = (e: React.ChangeEvent<HTMLInputElement>) => {setPseudo(e.target.value)}
   const callbackRoomChange = (e: React.ChangeEvent<HTMLInputElement>) => {setRoom(e.target.value)}
@@ -41,6 +49,11 @@ function App() {
         ? <GameBoard socket={socket} pseudo={pseudo} room={room} roomData={roomData} callbackLeaveRoom={callbackLeaveRoom} />
         : <Home socket={socket} callbackPseudoChange={callbackPseudoChange} callbackRoomChange={callbackRoomChange} callbackJoinRoom={callbackJoinRoom} />
       }
+      <Snackbar open={connectionError} autoHideDuration={6000} onClose={() => setConnectionError(false)}>
+          <Alert onClose={() => setConnectionError(false)} severity="error" sx={{ width: '100%' }}>
+              Impossible de se connecter au serveur. Veuillez contacter l'administrateur du jeu.
+          </Alert>
+      </Snackbar>
     </Box>
   )
 }
